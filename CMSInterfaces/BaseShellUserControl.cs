3b1d603eee9520fd5189798491de7827d25b835e
ninianne98 <ninianne98@gmail.com>
2011-10-22 00:20:28 +0000

    project interfaces

---
+++ CMSInterfaces/BaseShellUserControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+/*
+* CarrotCake CMS
+* http://carrotware.com/
+*
+* Copyright 2011, Samantha Copeland
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: October 2011
+*/
+
+
+namespace Carrotware.CMS.Interface {
+	public abstract class BaseShellUserControl : System.Web.UI.UserControl {
+
+		protected string CurrentDLLVersion {
+			get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
+		}
+
+
+		public string CurrentScriptName {
+			get { return Request.ServerVariables["script_name"].ToString(); }
+		}
+
+
+	}
+}
