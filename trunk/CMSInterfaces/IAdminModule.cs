64cb8dcc2621c2595df851d7cbbefa4a244eee98
ninianne98@gmail.com <ninianne98@gmail.com@483b25ac-5ad4-41e2-27e7-f6c672f185b2>
2011-10-22 00:20:28 +0000

    project interfaces

---
+++ trunk/CMSInterfaces/IAdminModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/*
+* CarrotCake CMS
+* http://carrotware.com/
+*
+* Copyright 2011, Samantha Copeland
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: October 2011
+*/
+
+namespace Carrotware.CMS.Interface {
+
+	public interface IAdminModule {
+
+		Guid SiteID { get; set; }
+
+        Guid ModuleID { get; set; }
+
+        string QueryStringFragment { get; set; }
+
+        string QueryStringPattern { get; set; }
+
+	}
+}
