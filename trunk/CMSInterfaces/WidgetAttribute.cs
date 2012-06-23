35b51ae5d473deaf223117f65967bbf362998acb
ninianne98@gmail.com <ninianne98@gmail.com@483b25ac-5ad4-41e2-27e7-f6c672f185b2>
2012-06-23 00:25:07 +0000

    added attribute tagging support to allow togging of checkbox list or dropdownlist so that naming/convention can be annoying

---
+++ trunk/CMSInterfaces/WidgetAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carrotware.CMS.Interface {
+	public class WidgetAttribute : Attribute {
+
+		public enum FieldMode {
+			DropDownList,
+			CheckBoxList,
+			TextBox,
+			CheckBox
+		}
+
+
+		public WidgetAttribute(FieldMode mode) {
+			this._mode = mode;
+		}
+
+		public WidgetAttribute(FieldMode mode, string field) {
+			this._mode = mode;
+			this._field = field;
+		}
+
+		private FieldMode _mode;
+		public FieldMode Mode {
+			get {
+				return this._mode;
+			}
+		}
+
+		private string _field;
+		public string SelectFieldSource {
+			get {
+				return this._field;
+			}
+		}
+
+
+	}
+}
