e3be94b82512774b1df546b15b3109f9e6ae45a9
ninianne98 <ninianne98@gmail.com>
2012-07-22 21:54:43 +0000

    big update to templates in the jquery ui look/feel - added enum to allow color/skin/theme toggling

--- WebControls/jquerybasic.cs
+++ WebControls/jquerybasic.cs
@@ -14,22 +14,68 @@ namespace Carrotware.Web.UI.Controls {
 	[ToolboxData("<{0}:jquerybasic runat=server></{0}:jquerybasic>")]
 	public class jquerybasic : BaseWebControl {
 
+		public enum jQueryTheme {
+			GlossyBlack,
+			Silver,
+			Purple,
+			Blue,
+			Green
+		}
+
+
+		[Bindable(true)]
+		[Category("Appearance")]
+		[DefaultValue("")]
+		[Localizable(true)]
+		public jQueryTheme SelectedSkin {
+			get {
+				string s = (string)ViewState["SelectedSkin"];
+				jQueryTheme c = (jQueryTheme)Enum.Parse(typeof(jQueryTheme), "GlossyBlack", true);
+				if (!string.IsNullOrEmpty(s)) {
+					try {
+						c = (jQueryTheme)Enum.Parse(typeof(jQueryTheme), s, true);
+					} catch (Exception ex) { }
+				}
+				return c;
+			}
+			set {
+				ViewState["SelectedSkin"] = value.ToString();
+			}
+		}
 
 		protected override void RenderContents(HtmlTextWriter output) {
 			string sJQFile = "";
 
-			sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui.css");
-
-			output.Write("<!-- JQuery CSS --> <link href=\"" + sJQFile + "\" type=\"text/css\" rel=\"stylesheet\" /> \r\n");
-
-			sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquery170.js");
+			sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquery172.js");
 
 			output.Write("<!-- JQuery --> <script src=\"" + sJQFile + "\" type=\"text/javascript\"></script> \r\n");
 
-			sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-1816.js");
+			sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jqueryui-1-8-21.js");
 
 			output.Write("<!-- JQuery UI --> <script src=\"" + sJQFile + "\" type=\"text/javascript\"></script> \r\n");
 
+			sJQFile = "";
+
+			switch (SelectedSkin) {
+				case jQueryTheme.Silver:
+					sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-silver.css");
+					break;
+				case jQueryTheme.Purple:
+					sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-purple.css");
+					break;
+				case jQueryTheme.Green:
+					sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-green.css");
+					break;
+				case jQueryTheme.Blue:
+					sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-blue.css");
+					break;
+				case jQueryTheme.GlossyBlack:
+				default:
+					sJQFile = Page.ClientScript.GetWebResourceUrl(this.GetType(), "Carrotware.Web.UI.Controls.jquerybasic.jquery-ui-black.css");
+					break;
+			}
+
+			output.Write("<!-- JQuery CSS  " + SelectedSkin.ToString() + " --> <link href=\"" + sJQFile + "\" type=\"text/css\" rel=\"stylesheet\" /> \r\n");
 
 		}
 
