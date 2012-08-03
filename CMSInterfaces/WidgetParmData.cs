e52d4a98899e2e79b8ae39b337cc5cb00525810c
ninianne98 <ninianne98@gmail.com>
2012-08-03 05:36:29 +0000

    added helper abstract class for when using the injected data since most consumption will be the same, why dup effort?

---
+++ CMSInterfaces/WidgetParmData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carrotware.CMS.Interface {
+	public class WidgetParmData : BaseShellUserControl, IWidgetParmData {
+
+		#region IWidgetParmData Members
+
+		private Dictionary<string, string> _parms = new Dictionary<string, string>();
+		public Dictionary<string, string> PublicParmValues {
+			get { return _parms; }
+			set { _parms = value; }
+		}
+
+		#endregion
+
+		protected string GetParmValue(string sKey) {
+			string ret = null;
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower() == sKey.ToLower()
+					   select c.Value).FirstOrDefault();
+			}
+
+			return ret;
+		}
+
+		protected string GetParmValue(string sKey, string sDefault) {
+			string ret = null;
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower() == sKey.ToLower()
+					   select c.Value).FirstOrDefault();
+			}
+
+			ret = ret == null ? sDefault : ret;
+
+			return ret;
+		}
+
+
+		protected List<string> GetParmValueList(string sKey) {
+
+			sKey = sKey.EndsWith("|") ? sKey : sKey + "|";
+			sKey = sKey.ToLower();
+
+			List<string> ret = new List<string>();
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower().StartsWith(sKey)
+					   select c.Value).ToList();
+			}
+
+			return ret;
+		}
+
+
+
+	}
+}
