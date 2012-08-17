0dbc3e48bef2a9799655cc3068aaa61e30a1d693
ninianne98 <ninianne98@gmail.com>
2012-08-17 03:44:45 +0000

    updated the data per naming and added meta data for images

--- CMSDataScripts/Schema Objects/Tables/dbo.tblGallery.table.sql
+++ CMSDataScripts/Schema Objects/Tables/dbo.tblGallery.table.sql
@@ -2,7 +2,7 @@
 	[GalleryID] [uniqueidentifier] NOT NULL,
 	[GalleryTitle] [varchar](255) NULL,
 	[SiteID] [uniqueidentifier] NULL,
- CONSTRAINT [tblGallery_PK_UC1] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [tblGallery_PK] PRIMARY KEY CLUSTERED 
 (
 	[GalleryID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
---
+++ CMSDataScripts/Schema Objects/Tables/dbo.tblGalleryImageMeta.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblGalleryImageMeta](
+	[GalleryImageMetaID] [uniqueidentifier] NOT NULL,
+	[GalleryImage] [varchar](512) NULL,
+	[ImageMetaData] [varchar](max) NULL,
+	[SiteID] [uniqueidentifier] NULL,
+ CONSTRAINT [tblGalleryImageMeta_PK] PRIMARY KEY CLUSTERED 
+(
+	[GalleryImageMetaID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
