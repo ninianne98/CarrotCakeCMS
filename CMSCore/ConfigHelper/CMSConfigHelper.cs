bc4d9969ba8e4dd42b23aa55ff9189c5746d887a
ninianne98 <ninianne98@gmail.com>
2012-08-28 23:59:32 +0000

    organize core classes into folders by purpose

--- CMSCore/ConfigHelper/CMSConfigHelper.cs
+++ CMSCore/ConfigHelper/CMSConfigHelper.cs
@@ -7,11 +7,9 @@ using System.Reflection;
 using System.Text;
 using System.Web;
 using System.Web.Caching;
-using System.Web.UI;
 using System.Xml.Serialization;
 using Carrotware.CMS.Data;
 using Carrotware.CMS.Interface;
-
 /*
 * CarrotCake CMS
 * http://www.carrotware.com/
@@ -24,15 +22,21 @@ using Carrotware.CMS.Interface;
 
 namespace Carrotware.CMS.Core {
 
-
-
-
 	public class CMSConfigHelper : IDisposable {
 
 		public CMSConfigHelper() {
 
 		}
 
+		private enum CMSConfigFileType {
+			AdminMod,
+			AdminModules,
+			PublicControls,
+			SkinDef,
+			PublicCtrl,
+			SiteSkins,
+			SiteMapping
+		}
 
 		private CarrotCMSDataContext db = new CarrotCMSDataContext();
 
@@ -87,16 +91,6 @@ namespace Carrotware.CMS.Core {
 
 				return domName.ToLower();
 			}
-		}
-
-		private enum CMSConfigFileType {
-			AdminMod,
-			AdminModules,
-			PublicControls,
-			SkinDef,
-			PublicCtrl,
-			SiteSkins,
-			SiteMapping
 		}
 
 		private static DataSet ReadDataSetConfig(CMSConfigFileType cfg, string sPath) {
@@ -843,76 +837,5 @@ namespace Carrotware.CMS.Core {
 
 
 
-	public class ObjectProperty {
-		public ObjectProperty() { }
-		public string Name { get; set; }
-		public bool CanWrite { get; set; }
-		public bool CanRead { get; set; }
-		public Type PropertyType { get; set; }
-
-		public Object DefValue { get; set; }
-
-		public PropertyInfo Props { get; set; }
-
-		public string CompanionSourceFieldName { get; set; }
-
-		public WidgetAttribute.FieldMode FieldMode { get; set; }
-
-
-		public override bool Equals(Object obj) {
-			//Check for null and compare run-time types.
-			if (obj == null || GetType() != obj.GetType()) return false;
-			if (obj is ObjectProperty) {
-				ObjectProperty p = (ObjectProperty)obj;
-				return (Name == p.Name) && (PropertyType == p.PropertyType);
-			} else {
-				return false;
-			}
-		}
-
-		public override int GetHashCode() {
-			return Name.GetHashCode() ^ PropertyType.ToString().GetHashCode();
-		}
-
-	}
-
-
-	public class CMSAdminModule {
-		public CMSAdminModule() {
-			PluginMenus = new List<CMSAdminModuleMenu>();
-		}
-		public Guid PluginID { get; set; }
-		public string PluginName { get; set; }
-		public List<CMSAdminModuleMenu> PluginMenus { get; set; }
-	}
-
-	public class CMSAdminModuleMenu {
-		public Guid PluginID { get; set; }
-		public int SortOrder { get; set; }
-		public string Caption { get; set; }
-		public string PluginParm { get; set; }
-		public string ControlFile { get; set; }
-		public bool UseAjax { get; set; }
-		public bool UsePopup { get; set; }
-		public bool IsVisible { get; set; }
-
-	}
-
-	public class CMSPlugin {
-		public string FilePath { get; set; }
-		public string Caption { get; set; }
-	}
-
-	public class CMSTemplate {
-		public string TemplatePath { get; set; }
-		public string Caption { get; set; }
-		public string EncodedPath { get; set; }
-	}
-
-	public class DynamicSite {
-		public Guid SiteID { get; set; }
-		public string DomainName { get; set; }
-
-	}
 
 }
---
+++ CMSCore/ConfigHelper/ObjectProperty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Carrotware.CMS.Interface;
+/*
+* CarrotCake CMS
+* http://www.carrotware.com/
+*
+* Copyright 2011, Samantha Copeland
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: October 2011
+*/
+
+namespace Carrotware.CMS.Core {
+
+	public class ObjectProperty {
+		public ObjectProperty() { }
+		public string Name { get; set; }
+		public bool CanWrite { get; set; }
+		public bool CanRead { get; set; }
+		public Type PropertyType { get; set; }
+
+		public Object DefValue { get; set; }
+
+		public PropertyInfo Props { get; set; }
+
+		public string CompanionSourceFieldName { get; set; }
+
+		public WidgetAttribute.FieldMode FieldMode { get; set; }
+
+
+		public override bool Equals(Object obj) {
+			//Check for null and compare run-time types.
+			if (obj == null || GetType() != obj.GetType()) return false;
+			if (obj is ObjectProperty) {
+				ObjectProperty p = (ObjectProperty)obj;
+				return (Name == p.Name) && (PropertyType == p.PropertyType);
+			} else {
+				return false;
+			}
+		}
+
+		public override int GetHashCode() {
+			return Name.GetHashCode() ^ PropertyType.ToString().GetHashCode();
+		}
+
+	}
+}
