40f0773b1f1f4d0016d669cde126a3edd33adb00
ninianne98 <ninianne98@gmail.com>
2012-09-23 07:31:37 +0000

    modified template mechanism

--- CMSControls/Properties/AssemblyInfo.cs
+++ CMSControls/Properties/AssemblyInfo.cs
@@ -43,7 +43,8 @@ using System.Web.UI;
 
 [assembly: TagPrefix("TwoLevelNavigation", "carrot")]
 [assembly: TagPrefix("TwoLevelNavigationTemplate", "carrot")]
-[assembly: TagPrefix("StructuredNavLink", "carrot")] 
+[assembly: TagPrefix("StructuredNavLink", "carrot")]
+[assembly: TagPrefix("ListItemHTMLWrap", "carrot")]
 
 [assembly: TagPrefix("MultiLevelNavigation", "carrot")]
 [assembly: TagPrefix("ChildNavigation", "carrot")]
