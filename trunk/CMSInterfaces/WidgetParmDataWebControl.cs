0963d968b23b4c1eaca3d98e0c753f3b780c8dfd
ninianne98@gmail.com <ninianne98@gmail.com@483b25ac-5ad4-41e2-27e7-f6c672f185b2>
2012-09-28 00:08:16 +0000

    added IActivateNavItem for common control handling, updated BaseServerControl to include the two interfaces commonly used in the CMS controls

---
+++ trunk/CMSInterfaces/WidgetParmDataWebControl.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace Carrotware.CMS.Interface {
+	public class WidgetParmDataWebControl : WebControl, IWidget, IWidgetParmData {
+
+		#region IWidget Members
+
+		public Guid PageWidgetID { get; set; }
+
+		public Guid RootContentID { get; set; }
+
+		public Guid SiteID { get; set; }
+
+
+		public string JSEditFunction {
+			get { return ""; }
+		}
+		public bool EnableEdit {
+			get { return true; }
+		}
+		#endregion
+
+
+		#region IWidgetParmData Members
+
+		private Dictionary<string, string> _parms = new Dictionary<string, string>();
+		public Dictionary<string, string> PublicParmValues {
+			get { return _parms; }
+			set { _parms = value; }
+		}
+
+		#endregion
+
+		protected string GetParmValue(string sKey) {
+			string ret = null;
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower() == sKey.ToLower()
+					   select c.Value).FirstOrDefault();
+			}
+
+			return ret;
+		}
+
+		protected string GetParmValue(string sKey, string sDefault) {
+			string ret = null;
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower() == sKey.ToLower()
+					   select c.Value).FirstOrDefault();
+			}
+
+			ret = ret == null ? sDefault : ret;
+
+			return ret;
+		}
+
+		protected List<string> GetParmValueList(string sKey) {
+
+			sKey = sKey.EndsWith("|") ? sKey : sKey + "|";
+			sKey = sKey.ToLower();
+
+			List<string> ret = new List<string>();
+
+			if (PublicParmValues.Count > 0) {
+				ret = (from c in PublicParmValues
+					   where c.Key.ToLower().StartsWith(sKey)
+					   select c.Value).ToList();
+			}
+
+			return ret;
+		}
+
+
+
+	}
+}
