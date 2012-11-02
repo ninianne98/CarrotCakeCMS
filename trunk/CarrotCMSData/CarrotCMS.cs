934e2dad15cf5d2cda9055a068bf4eb1d1ec3048
ninianne98@gmail.com <ninianne98@gmail.com@483b25ac-5ad4-41e2-27e7-f6c672f185b2>
2012-11-02 04:57:30 +0000

    update to avoid any future issues between web vs app configs

--- trunk/CarrotCMSData/CarrotCMS.cs
+++ trunk/CarrotCMSData/CarrotCMS.cs
@@ -1,10 +1,20 @@
 using System;
+using System.Configuration;
 
 namespace Carrotware.CMS.Data {
 	public partial class CarrotCMSDataContext {
 
 		public static CarrotCMSDataContext GetDataContext() {
-			CarrotCMSDataContext _db = new CarrotCMSDataContext();
+			CarrotCMSDataContext _db = null;
+
+			_db = GetDataContext(ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString);
+
+			//if (_db.Connection.ConnectionString.IndexOf("localhost") > 0) {
+			//	_db = GetDataContext(ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString);
+			//} else {
+			//    _db = new CarrotCMSDataContext();
+			//}
+
 #if DEBUG
 			_db.Log = new DebugTextWriter();
 #endif
@@ -28,6 +38,18 @@ namespace Carrotware.CMS.Data {
 #endif
 			return _db;
 		}
+
+
+
+		//public CarrotCMSDataContext() :
+		//    base(global::Carrotware.CMS.Data.Properties.Settings.Default.CarrotwareCMSConnectionString, mappingSource) {
+		//    OnCreated();
+		//}
+
+		//public CarrotCMSDataContext() :
+		//    base(global::System.Configuration.ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString, mappingSource) {
+		//    OnCreated();
+		//}
 
 	}
 }
