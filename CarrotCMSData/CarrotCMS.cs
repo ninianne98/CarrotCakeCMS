a8fe4a40e46a57e9114802968dd56706a2f867ec
ninianne98 <ninianne98@gmail.com>
2012-11-09 18:56:07 +0000

    further refinements of compiled query usage

--- CarrotCMSData/CarrotCMS.cs
+++ CarrotCMSData/CarrotCMS.cs
@@ -1,42 +1,45 @@
 using System;
 using System.Configuration;
+using System.Data;
+/*
+* CarrotCake CMS
+* http://www.carrotware.com/
+*
+* Copyright 2011, Samantha Copeland
+* Dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Date: October 2011
+*/
+
 
 namespace Carrotware.CMS.Data {
 	public partial class CarrotCMSDataContext {
 
+		private static string connString = ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString;
+
 		public static CarrotCMSDataContext GetDataContext() {
-			CarrotCMSDataContext _db = null;
 
-			_db = GetDataContext(ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString);
-
-			//if (_db.Connection.ConnectionString.IndexOf("localhost") > 0) {
-			//	_db = GetDataContext(ConfigurationManager.ConnectionStrings["CarrotwareCMSConnectionString"].ConnectionString);
-			//} else {
-			//    _db = new CarrotCMSDataContext();
-			//}
-
-#if DEBUG
-			_db.Log = new DebugTextWriter();
-#endif
-			return _db;
+			return GetDataContext(connString);
 		}
 
 
 		public static CarrotCMSDataContext GetDataContext(string connection) {
-			CarrotCMSDataContext _db = new CarrotCMSDataContext(connection);
 #if DEBUG
+			CarrotCMSDataContext _db = new CarrotCMSDataContext(connection);
 			_db.Log = new DebugTextWriter();
-#endif
 			return _db;
+#endif
+			return new CarrotCMSDataContext(connection);
 		}
 
 
-		public static CarrotCMSDataContext GetDataContext(System.Data.IDbConnection connection) {
-			CarrotCMSDataContext _db = new CarrotCMSDataContext(connection);
+		public static CarrotCMSDataContext GetDataContext(IDbConnection connection) {
 #if DEBUG
+			CarrotCMSDataContext _db = new CarrotCMSDataContext(connection);
 			_db.Log = new DebugTextWriter();
-#endif
 			return _db;
+#endif
+			return new CarrotCMSDataContext(connection);
 		}
 
 
