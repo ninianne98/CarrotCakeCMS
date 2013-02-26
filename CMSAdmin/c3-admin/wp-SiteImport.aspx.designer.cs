81096c66c3915149c376440b6867acd6ffe870eb
ninianne98 <ninianne98@gmail.com>
2013-02-26 05:21:07 +0000

    fix issue where wordpress imported categories and tags are imported as private when doing a wordpress import, added validation that a folder is selected when the box to download is checked

--- CMSAdmin/c3-admin/wp-SiteImport.aspx.designer.cs
+++ CMSAdmin/c3-admin/wp-SiteImport.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Carrotware.CMS.UI.Admin.c3_admin {
         protected global::System.Web.UI.WebControls.Literal litTrust;
         
         /// <summary>
+        /// CustomValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator CustomValidator1;
+        
+        /// <summary>
         /// ddlFolders control.
         /// </summary>
         /// <remarks>
